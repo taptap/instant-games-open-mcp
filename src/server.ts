ea137b5ecb4d543fe0c7851f00b820bfb2aa22ae
MikotoZero <ding3725371@hotmail.com>
2025-10-20 17:28:10 +0800

    chore: bump version to 1.2.0-beta.6

--- src/server.ts
+++ src/server.ts
@@ -58,7 +58,7 @@ class TapTapMinigameMCPServer {
     this.server = new Server(
       {
         name: 'taptap-minigame-mcp',
-        version: '1.2.0-beta.5',
+        version: '1.2.0-beta.6',
       }
     );
 
